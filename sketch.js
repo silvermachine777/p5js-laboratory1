023a083599c1fdf3d32f6f5af30b58908db02576
silvermachine777 <sebastian_gg@outlook.com>
2023-12-27 17:14:37 -0500

    with a heart

--- sketch.js
+++ sketch.js
@@ -4,9 +4,15 @@ function setup() {
 
 function draw() {
     if (mouseIsPressed) {
-        fill(0);
+        fill(255, 0, 0); 
     } else {
-        fill(255);
+        fill(255); 
     }
-    ellipse(mouseX, mouseY, 80, 80);
+
+    
+    beginShape();
+    vertex(mouseX, mouseY - 50);
+    bezierVertex(mouseX + 50, mouseY - 80, mouseX + 90, mouseY - 40, mouseX, mouseY + 50);
+    bezierVertex(mouseX - 90, mouseY - 40, mouseX - 50, mouseY - 80, mouseX, mouseY - 50);
+    endShape(CLOSE);
 }
